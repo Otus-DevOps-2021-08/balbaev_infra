ec7e3254397fff79d38548fe52bdde8c2ea941f6
Zhan Balbaev <balbaev@selectel.ru>
2022-02-23 17:13:50 +0300

    Add TF modules

---
+++ terraform/modules/db/main.tf
@@ -0,0 +1,29 @@
+resource "yandex_compute_instance" "db" {
+  name = "reddit-db-${var.env}"
+
+  labels = {
+    tags = "reddit-db"
+    env  = var.env
+  }
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.db_disk_image
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+}
