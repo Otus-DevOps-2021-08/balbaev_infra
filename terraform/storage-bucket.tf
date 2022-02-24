8794e5bd614f45f200b31f5a72805e96cd165338
Zhan Balbaev <balbaev@selectel.ru>
2022-02-24 04:47:35 +0300

    Add storage bucket

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = var.bucket_name
+    region     = "us-east-1"
+    key        = "terraform.tfstate"
+    access_key = var.access_key
+    secret_key = var.secret_key
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
